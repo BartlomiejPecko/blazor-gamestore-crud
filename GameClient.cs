3c104c8a391900d80e1c47964f77969c90928bb6
Bartłomiej Pecko <bartlomiejpecko@gmail.com>
2023-10-20 01:03:49 +0200

    Added bootstrap to table Created GameClient

---
+++ GameClient.cs
@@ -0,0 +1,42 @@
+﻿using blazorgamestorecrud.Models;
+
+namespace blazorgamestorecrud;
+
+public static class GameClient
+{
+    private static readonly List<Game> games = new()
+    {
+        new Game()
+        {
+            Id = 1,
+            Name = "FirstGame",
+            Genre = "FirstGenre",
+            Price = 19.99M,
+            ReleaseDate = new DateTime(2013, 2, 1)
+        },
+
+        new Game()
+        {
+            Id = 2,
+            Name = "SecondGame",
+            Genre = "SecondGenre",
+            Price = 19.99M,
+            ReleaseDate = new DateTime(1996, 2, 1)
+        },
+
+        new Game()
+        {
+            Id = 2,
+            Name = "ThirdGame",
+            Genre = "ThirdGenre",
+            Price = 29.99M,
+            ReleaseDate = new DateTime(1998, 2, 1)
+        },
+
+    };
+
+    public static Game[] GetGames()
+    {
+        return games.ToArray();
+    }
+}
